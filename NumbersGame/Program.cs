8db2379e95f62b64aeb2c11cd8f0e79266b85903
Madde Lundström <madde.lundstrom@edu.edugrade.se>
2022-09-29 14:09:12 +0200

    Klar

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -6,12 +6,46 @@ namespace NumbersGame
     {
         static void Main(string[] args)
         {
+            StartGame();
+            EndOfGame();
+        }
+        private static void StartGame()
+        {
+            Console.Clear();
             Random rand = new Random();
-            int x = 1;
-            int y = 20;
-            int timesGuess = 5;
-            int number = rand.Next(x, y);
-            Console.WriteLine("Välkommen! Jag tänker på ett nummer mellan {0} och {1}. Kan du gissa vilket? Du får {2} försök.", x, y, timesGuess);
+            Console.WriteLine("Välkommen, låt oss spela ett spel. Välj svårighetsgrad mellan 1-5:");
+            while (true)
+            {
+                int userDifficulty = CheckIfInt();
+                switch (userDifficulty)
+                {
+                    case 1:
+                        Difficulty(1, 20, 5);
+                        break;
+                    case 2:
+                        Difficulty(1, 30, 6);
+                        break;
+                    case 3:
+                        Difficulty(1, 55, 7);
+                        break;
+                    case 4:
+                        Difficulty(1, 75, 8);
+                        break;
+                    case 5:
+                        Difficulty(1, 100, 10);
+                        break;
+                    default:
+                        Console.WriteLine("Du måste skriva ett heltal mellan 1-5");
+                        break;
+                }
+            }
+        }
+        private static void Difficulty(int low, int high, int timesGuess)
+        {
+            Console.Clear();
+            Random rand = new Random();
+            int number = rand.Next(low, high);
+            Console.WriteLine("Jag tänker på ett nummer mellan {0} och {1}. Kan du gissa vilket? Du får {2} försök.", low, high, timesGuess);
             CheckGuess(number, timesGuess);
         }
         private static void CheckGuess(int number, int timesGuess)
@@ -21,23 +55,41 @@ namespace NumbersGame
                 int userGuess = CheckIfInt();
                 if (userGuess == number)
                 {
-                    Console.WriteLine("Woho! Du gjorde! \nTryck Enter för att avsluta");
-                    Console.ReadKey();
-                    Environment.Exit(0);
+                    ResposeWin();
+                    EndOfGame();
                 }
                 else if (userGuess < number)
                 {
-                    Console.WriteLine("Tyvärr du gissade för lågt!");
+                    ResponseLow();
                 }
                 else if (userGuess > number)
                 {
-                    Console.WriteLine("Tyvärr du gissade för högt!");
+                    ResponseHigh();
                 }
             }
             Console.WriteLine("Tyvärr du lyckades inte gissa talet på fem försök!");
-            Console.WriteLine("Programmet kommer att avslutas när du trycker på Enter");
-            Console.ReadKey();
-            Environment.Exit(0);
+            EndOfGame();
+        }
+        private static void EndOfGame()
+        {
+            Console.WriteLine("Vill du spela igen? (ja/nej)");
+            while (true)
+            {
+                string restart = Console.ReadLine();
+                if (restart == "ja")
+                {
+                    StartGame();
+                }
+                else if (restart == "nej")
+                {
+                    Console.WriteLine("Spelet avslutas");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Du måste skriva ja eller nej, försök igen");
+                }
+            }
         }
         private static int CheckIfInt()
         {
@@ -47,6 +99,42 @@ namespace NumbersGame
                 Console.WriteLine("Du måste skriva in ett heltal, försök igen.");
             }
             return userInt;
+        }
+        private static void ResposeWin()
+        {
+            Random randResponse = new Random();
+            string[] responseWin = new string[5];
+            responseWin[0] = "Grattis du gissade rätt!";
+            responseWin[1] = "Whooho du vann!";
+            responseWin[2] = "Bra jobbat! Du gissade rätt!";
+            responseWin[3] = "Awesome, rätt nummer!";
+            responseWin[4] = "YES! Helt rätt!";
+            int indexWin = randResponse.Next(responseWin.Length);
+            Console.WriteLine(responseWin[indexWin]);
+        }
+        private static void ResponseLow()
+        {
+            Random randResponse = new Random();
+            string[] responseLow = new string[5];
+            responseLow[0] = "Nope, det var för lågt";
+            responseLow[1] = "Du måste gissa högre";
+            responseLow[2] = "Bra gissat men det var för lågt";
+            responseLow[3] = "Nej nej, högre";
+            responseLow[4] = "Nope, du behöver gissa högre än så";
+            int indexLow = randResponse.Next(responseLow.Length);
+            Console.WriteLine(responseLow[indexLow]);
+        }
+        private static void ResponseHigh()
+        {
+            Random randResponse = new Random();
+            string[] responseHigh = new string[5];
+            responseHigh[0] = "Nope, det var för högt";
+            responseHigh[1] = "Du måste gissa lägre";
+            responseHigh[2] = "Bra gissat men det var för högt";
+            responseHigh[3] = "Haha, nej nej lägre";
+            responseHigh[4] = "Kunde ha varit, men det är lägre än så";
+            int indexHigh = randResponse.Next(responseHigh.Length);
+            Console.WriteLine(responseHigh[indexHigh]);
         }
     }
 }
